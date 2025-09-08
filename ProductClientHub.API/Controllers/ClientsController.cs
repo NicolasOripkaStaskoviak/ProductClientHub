246470f897f1b8ae038baf1943d5a550a5502cc2
NicolasOripkaStaskoviak <nicolas.staskoviak@gmail.com>
2025-09-08 13:58:55 -0300

    Iniciando o tratamento de erros com try catch

--- ProductClientHub.API/Controllers/ClientsController.cs
+++ ProductClientHub.API/Controllers/ClientsController.cs
@@ -11,13 +11,26 @@ namespace ProductClientHub.API.Controllers
     {
         [HttpPost]
         [ProducesResponseType(typeof(ResponseClientJson), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(ResponseErrorMessagesJson), StatusCodes.Status400BadRequest)]
         public IActionResult Register([FromBody]RequestClientJson request)
         {
-            var useCase = new UseCases.Clients.Register.RegisterClientUseCase();
+            try {
+                var useCase = new UseCases.Clients.Register.RegisterClientUseCase();
 
-            var response = useCase.Execute(request);
+                var response = useCase.Execute(request);
 
-            return Created(string.Empty, response);
+                return Created(string.Empty, response);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(new ResponseErrorMessagesJson(ex.Message));
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new ResponseErrorMessagesJson("ERRO DESCONHECIDO"));
+
+            }
+
         }
 
         [HttpPut]
